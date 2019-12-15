56f771e39b269fe610040e155777338e7ab5cc44
Bartosz <kowalczykbar98@gmail.com>
2019-12-15 14:17:37 +0100

    dodany popout jak najedziesz na znak
    po najechaniu kursorem na znak pojawia sie popout z info o znaku i jak zjedziemy kursoerm to po 0.5s znika można to zbugować ale mniejsza o to

---
+++ Assets/pokazZnak.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class pokazZnak : MonoBehaviour
+{
+    public Canvas infoOZnaku;
+    public float delay = 0.5f;
+    public void showInfo()
+    {
+        infoOZnaku.enabled = true;
+    }
+    public void hideInfo()
+    {
+        Invoke("disable", delay);
+    }
+    private void disable()
+    {
+        infoOZnaku.enabled = false;
+    }
+}
