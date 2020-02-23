5533f6ec2a5440d58fdc8acee836487cbedf480e
BartoszKowalczyk98 <44229112+BartoszKowalczyk98@users.noreply.github.com>
2020-02-23 18:40:32 +0100

    zladrogajakotag

--- Assets/scripts/Endtrigger.cs
+++ Assets/scripts/Endtrigger.cs
@@ -6,7 +6,10 @@ public class Endtrigger : MonoBehaviour
     public GameManager gameManager;
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(this.CompareTag("zladroga") && other.CompareTag("Player")){
+            gameManager.Invoke("Restart", gameManager.restartdelay);
+        }
+        else if(other.CompareTag("Player"))
             gameManager.CompleteLevel();
     }
 }
